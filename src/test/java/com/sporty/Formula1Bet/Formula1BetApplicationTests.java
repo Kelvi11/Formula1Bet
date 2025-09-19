536f4135fcd5af7d7e75e25e52abe7a62ce530be
Kelvin Dervishllari <kelvin.dervishllari.ext@sisal.al>
2025-09-19 11:43:22 +0200

    introduce spring boot

---
+++ src/test/java/com/sporty/Formula1Bet/Formula1BetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sporty.Formula1Bet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1BetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

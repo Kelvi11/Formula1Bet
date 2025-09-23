a44cc304479ecaf4130f33cdd936f054b916dbf6
Kelvin Dervishllari <kelvin.dervishllari.ext@sisal.al>
2025-09-23 17:27:35 +0200

    cleaning and refactoring

--- src/main/java/com/sporty/formula1bet/repository/bet/BetRepositoryImpl.java
+++ src/main/java/com/sporty/formula1bet/repository/bet/BetRepositoryImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 
 @Component
 public class BetRepositoryImpl implements BetRepository {
